a9b27e89048bf1d706a8881ea536392681763621
sajdakabir <sajda.kbir.com>
2022-06-10 11:56:46 +0530

    Roman Numeral To Integer

---
+++ roman_numeral_to_intger.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+int romanToInt(string s) {
+    // Write your code here
+        unordered_map<char, int> mp = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+    int res = 0;
+    for(int i = 0; i < s.size() ; i++) {
+        if(mp[s[i]] < mp[s[i + 1]]) res -= mp[s[i]];
+        else res += mp[s[i]];
+    }
+    return res;
+}

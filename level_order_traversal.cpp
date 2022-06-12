19a0543a843f40d1ce63734f3a23f013eaadd762
sajdakabir <sajda.kbir.com>
2022-06-12 20:47:29 +0530

    level order traversal

---
+++ level_order_traversal.cpp
@@ -0,0 +1,22 @@
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+
+    vector<int>temp;
+        if(root==NULL)return temp;
+        queue<BinaryTreeNode<int>*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            
+            for(int i=0;i<size;i++){
+               BinaryTreeNode<int>* node=q.front();
+                q.pop();
+                if(node->left!=NULL)q.push(node->left);
+                if(node->right!=NULL)q.push(node->right);
+                temp.push_back(node->val);
+            }
+
+        }
+        return temp;
+
+}

7564fc5a6f0fe2d866b3649c9fd09d512b5879da
sajdakabir <sajda.kbir.com>
2022-06-14 15:56:52 +0530

    LCA of Two Nodes In A BST

---
+++ LCA_of_two_nodesInABST.cpp
@@ -0,0 +1,18 @@
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* p, TreeNode<int>* q)
+{
+	// Write your code here
+    if (root == NULL) {
+            return NULL;
+        }
+        
+        int curr = root->data;
+        
+        if (curr < p->data && curr < q->data) {
+            return LCAinaBST(root->right, p, q);
+        }
+        if (curr > p->data && curr > q->data) {
+            return LCAinaBST(root->left, p, q);
+        }
+        return root;
+}

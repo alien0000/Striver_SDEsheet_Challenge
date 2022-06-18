646017075fc9bda837d8e1050902fa695256bacf
sajdakabir <sajda.kbir.com>
2022-06-19 00:38:27 +0530

    delete node in bst

---
+++ deleteNodeInBst.cpp
@@ -0,0 +1,75 @@
+/*************************************************************
+    Following is the Binary Tree node structure
+
+    template <typename T>
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() {
+            if (left)
+                delete left;
+            if (right)
+                delete right;
+        }
+    };
+
+*************************************************************/
+BinaryTreeNode<int> *findLastRight(BinaryTreeNode<int> *root)
+{
+    if(root->right == NULL)
+        return root;
+    return findLastRight(root->right);
+}
+BinaryTreeNode<int>* helper(BinaryTreeNode<int> *root)
+{
+    if(root->right == NULL)
+        return root->left;
+    else if(root->left == NULL)
+        return root->right;
+    BinaryTreeNode<int> *rightChild = root->right;
+    BinaryTreeNode<int> *lastRight = findLastRight(root->left);
+    lastRight->right = rightChild;
+    return root->left;
+}
+BinaryTreeNode<int>* deleteNode(BinaryTreeNode<int>* root, int key){
+    if(root == NULL) return NULL;
+    if(root->data == key)
+        return helper(root);
+    BinaryTreeNode<int> *temp = root;
+    while(root)
+    {
+        if(root->data > key)
+        {
+            if(root->left != NULL && root->left->data == key)
+            {
+                root->left = helper(root->left);
+                break;
+             }
+            else
+                root = root->left;
+          }
+          else
+          {
+                  if(root->right != NULL && root->right->data == key)
+                {
+                    root->right = helper(root->right);
+                    break;
+                }
+                else
+                    root = root->right;
+            }        
+     }
+     return temp;
+}
+
